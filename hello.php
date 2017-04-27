efe0b802a13af870a3ff633c63f2af88e396196a
miaopass <492585385@qq.com>
2017-04-27 17:45:42 +0800

    1

--- hello.php
+++ hello.php
@@ -1,9 +1,10 @@
-﻿<!DOCTYPE html>
-<html>
-<body>
-<?php
-echo "中文Hello World!";
-?>  
+﻿<?php
 
-</body>
-</html>
+$attr = file_get_contents('php://input');
+$xml = simplexml_load_string($attr);
+echo $xml->accout;
+echo "中文Hello World!";
+?>
+
+
+
