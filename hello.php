20839ce2eb824e4c0d1a6eab6cee406d2aa7e556
miaopass <492585385@qq.com>
2017-04-27 22:47:23 +0800

    1

--- hello.php
+++ hello.php
@@ -1,10 +1,22 @@
 ﻿<?php
+$str="<?xml version='1.0'?>
+<object>
+<deviceid>010002</deviceid>
+<custid>0001</custid>
+</object>";
 
-$attr = file_get_contents('php://input');
-$xml = simplexml_load_string($attr);
-echo $xml->accout;
-echo "中文Hello World!";
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL,"http://127.0.0.1:8080/hello1.php");
+
+curl_setopt($ch, CURLOPT_POST, true);
+
+curl_setopt($ch, CURLOPT_POSTFIELDS, $str);
+
+$response = curl_exec($ch);
+$xml = simplexml_load_string($response);
+$custid= $xml->custid;
+if($custid=='0001'){
+    echo "成功了";
+}
 ?>
-
-
-
--- hello1.php
+++ hello1.php
@@ -1,9 +1,14 @@
-﻿<!DOCTYPE html>
-<html>
-<body>
 <?php
-echo "中文Hello World!";
-?>  
+$file_in = file_get_contents("php://input");
+$xml = simplexml_load_string($file_in);
 
-</body>
-</html>
+$password= $xml->deviceid;
+if($password =='010002'){
+$str="<?xml version='1.0'?>
+<object>
+<custid>0001</custid>
+</object>";
+echo $str;
+}
+
+?>
